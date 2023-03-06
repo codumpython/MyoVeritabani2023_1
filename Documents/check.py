a54b5be1b6d94774694a07d8c6e37058e3aaeb63
İbrahim EDİZ <pythonegitimiediz@gmail.com>
2023-03-06 10:35:59 +0000

    06032023_2

--- Documents/check.py
+++ Documents/check.py
@@ -1,13 +1,13 @@
 import os
 liste = os.listdir("/workspace/MyoVeritabani2023_1/Exercises")
 # print(len(liste))
-fileName = "ornek.sql"
+fileName = "select1.sql"
 for item in liste:
     open(f"/workspace/MyoVeritabani2023_1/Exercises/{item}/{fileName}","a+")
 
-for item in liste:
-    with open(f"/workspace/MyoVeritabani2023_1/Exercises/{item}/{fileName}","r") as dosya1,\
-        open(f"/workspace/MyoVeritabani2023_1/Exercises/IBRAHIMEDIZ/{fileName}","r") as dosya2:
-            cevap = dosya1.read()
-            if cevap != dosya2.read():
-                print(item)
+# for item in liste:
+#     with open(f"/workspace/MyoVeritabani2023_1/Exercises/{item}/{fileName}","r") as dosya1,\
+#         open(f"/workspace/MyoVeritabani2023_1/Exercises/IBRAHIMEDIZ/{fileName}","r") as dosya2:
+#             cevap = dosya1.read()
+#             if cevap != dosya2.read():
+#                 print(item)
