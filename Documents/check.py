64e8259334410b25136bcece0882554d9838c362
İbrahim EDİZ <pythonegitimiediz@gmail.com>
2023-03-20 11:06:32 +0000

    20032023
    Co-authored-by: İbrahim EDİZ <pythonegitimiediz@gmail.com>

--- Documents/check.py
+++ Documents/check.py
@@ -1,16 +1,14 @@
 import os
 liste = os.listdir("/workspace/MyoVeritabani2023_1/Exercises")
 # print(len(liste))
-fileName = "orderby.sql"
+fileName = "insert1.sql"
 for item in liste:
     metin = """
 /*
 ----Egzersiz----
-Veritabanında bulunan customers tablosunda yer alan verilerden
-State sütunu dolu olan ve Fax sütunu boş olan CustomerId sütunu
-20 ile 25 arasında olan verileri CustomerId verileri büyükten 
-küçüğe doğru sıralanacak şekilde verileri getiren sql sorgusunu
-yazınız
+Veritabanında bulunan insertegzersiz tablosun içerisine
+Firstname,Lastname,Email ve Number sütunlarına veri girişi yaparak
+kendi bilgileriniz kaydediniz. 
 */
 """
     print(metin,file=open(f"/workspace/MyoVeritabani2023_1/Exercises/{item}/{fileName}","a+"))
---
+++ Exercises/TOLGAATMACA/insert1.sql
@@ -0,0 +1,8 @@
+
+/*
+----Egzersiz----
+Veritabanında bulunan insertegzersiz tablosun içerisine
+Firstname,Lastname,Email ve Number sütunlarına veri girişi yaparak
+kendi bilgileriniz kaydediniz. 
+*/
+
