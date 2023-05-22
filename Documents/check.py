4dba56b50752693edf109173c531ccd6634facc6
İbrahim EDİZ <pythonegitimiediz@gmail.com>
2023-05-22 10:17:05 +0000

    22052023_1

--- Documents/check.py
+++ Documents/check.py
@@ -2,15 +2,15 @@ import os
 import random as rnd
 liste = os.listdir("/workspace/MyoVeritabani2023_1/Exercises")
 # print(len(liste))
-fileName = "join2.sql"
+fileName = "createview1.sql"
 for item in liste:
-    mini,maxi = sorted(rnd.sample([i for i in range(1,3503)],2))
+    
     metin = f"""
 /*
 ----Egzersiz----
 diagram.png dosyasında faydalanarak 
-Playlist ve içerisinde bulunan parçaların adlarını albüm isimleriyle birlikte parça isimlerini listeleyen sorguyu yazınız
-Bu parçalardan id si {mini} ile {maxi} arasında yer alan kayıtları listelemek için sorgu sonuna şart yazınız
+PlaylistID,PlaylistADI,ParcaADI,AlbumADI,SanatciADI
+yukarıdaki bilgileri bize getiren sorguyu yazınız
 */
 """
     print(metin,file=open(f"/workspace/MyoVeritabani2023_1/Exercises/{item}/{fileName}","w+"))
---
+++ Exercises/TOLGAATMACA/createview1.sql
@@ -0,0 +1,8 @@
+
+/*
+----Egzersiz----
+diagram.png dosyasında faydalanarak 
+PlaylistID,PlaylistADI,ParcaADI,AlbumADI,SanatciADI
+yukarıdaki bilgileri bize getiren sorguyu yazınız
+*/
+
